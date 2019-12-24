5090067004f40ab0124fb8f60885cc77be6ef4b5
Priyank Singh <priyanksingh8@gmail.com>
2019-12-25 01:18:06 +0530

    Added modal
    TODO:
    1. Add menu and other headings
    2. Correction in names of corrdinators
    3. Add svgs

--- scripts/events.js
+++ scripts/events.js
@@ -1,51 +1,194 @@
-/*
- * Noel Delgado | @pixelia_me
- */
+import eventsContent from "./eventsData.js";
 
-var items = [],
-  point = document.querySelector("svg").createSVGPoint();
+//modal
 
-function getCoordinates(e, svg) {
-  point.x = e.clientX;
-  point.y = e.clientY;
-  return point.matrixTransform(svg.getScreenCTM().inverse());
-}
+var body = document.body;
+var modal = createModal(document.querySelector("#modal-1"));
+var targetClicked = undefined;
 
-function changeColor(e) {
-  document.body.className = e.currentTarget.className;
-}
+//modal animations
 
-function Item(config) {
-  Object.keys(config).forEach(function(item) {
-    this[item] = config[item];
-  }, this);
-  this.el.addEventListener("mousemove", this.mouseMoveHandler.bind(this));
-  this.el.addEventListener("touchmove", this.touchMoveHandler.bind(this));
-}
+function createModal(container) {
+  var content = container.querySelector(".modal-content");
+  var dialog = container.querySelector(".modal-dialog");
+  var polygon = container.querySelector(".modal-polygon");
+  var svg = container.querySelector(".modal-svg");
 
-Item.prototype = {
-  update: function update(c) {
-    this.clip.setAttribute("cx", c.x);
-    this.clip.setAttribute("cy", c.y);
-  },
-  mouseMoveHandler: function mouseMoveHandler(e) {
-    this.update(getCoordinates(e, this.svg));
-  },
-  touchMoveHandler: function touchMoveHandler(e) {
-    e.preventDefault();
-    var touch = e.targetTouches[0];
-    if (touch) return this.update(getCoordinates(touch, this.svg));
+  var point1 = createPoint(45, 45);
+  var point2 = createPoint(55, 45);
+  var point3 = createPoint(55, 55);
+  var point4 = createPoint(45, 55);
+
+  var animation = new TimelineMax({
+    onReverseComplete: onReverseComplete,
+    onStart: onStart,
+    paused: true
+  })
+    .to(
+      point1,
+      0.3,
+      {
+        x: 15,
+        y: 30,
+        ease: Power4.easeIn
+      },
+      0
+    )
+    .to(
+      point4,
+      0.3,
+      {
+        x: 5,
+        y: 80,
+        ease: Power2.easeIn
+      },
+      "-=0.1"
+    )
+    .to(point1, 0.3, {
+      x: 0,
+      y: 0,
+      ease: Power3.easeIn
+    })
+    .to(
+      point2,
+      0.3,
+      {
+        x: 100,
+        y: 0,
+        ease: Power2.easeIn
+      },
+      "-=0.2"
+    )
+    .to(point3, 0.3, {
+      x: 100,
+      y: 100,
+      ease: Power2.easeIn
+    })
+    .to(
+      point4,
+      0.3,
+      {
+        x: 0,
+        y: 100,
+        ease: Power2.easeIn
+      },
+      "-=0.1"
+    )
+    .to(
+      container,
+      1,
+      {
+        autoAlpha: 1
+      },
+      0
+    )
+    .to(content, 1, {
+      autoAlpha: 1
+    });
+
+  var modal = {
+    animation: animation,
+    container: container,
+    content: content,
+    dialog: dialog,
+    isOpen: false,
+    open: open,
+    close: close
+  };
+
+  body.removeChild(container);
+
+  function onClick() {
+    if (modal.isOpen) {
+      close();
+    }
   }
-};
 
-[].slice
-  .call(document.querySelectorAll(".thumbnail"), 0)
-  .forEach(function(item, index) {
-    items.push(
-      new Item({
-        el: item,
-        svg: item.querySelector("svg"),
-        clip: document.querySelector("#clip-" + index + " circle")
-      })
-    );
+  function onStart() {
+    body.appendChild(container);
+    const targetName = targetClicked.innerHTML;
+    console.log(targetName);
+    const modalHeader = container.querySelector(".modal-header");
+    const modalBody = container.querySelector(".modal-body");
+    modalHeader.innerHTML = eventsContent[targetName].header;
+    modalBody.innerHTML = eventsContent[targetName].body;
+    container.addEventListener("click", onClick);
+  }
+
+  function onReverseComplete() {
+    container.removeEventListener("click", onClick);
+    body.removeChild(container);
+  }
+
+  function open() {
+    modal.isOpen = true;
+    animation.play().timeScale(2);
+  }
+
+  function close() {
+    modal.isOpen = false;
+    animation.reverse().timeScale(2.5);
+  }
+
+  function createPoint(x, y) {
+    var point = polygon.points.appendItem(svg.createSVGPoint());
+    point.x = x || 0;
+    point.y = y || 0;
+    return point;
+  }
+
+  return modal;
+}
+
+let timeout, imgto;
+const ul = document.getElementById("items");
+const li = document.getElementsByTagName("li");
+const cap = document.getElementById("event-img");
+const ar_li = [...li];
+ul.addEventListener("mouseover", e => {
+  const el = e.target;
+  cap.style.opacity = 0;
+  if (el.tagName === "LI") {
+    console.log(el);
+    clearTimeout(imgto);
+
+    imgto = setTimeout(() => {
+      cap.setAttribute("src", `assets/events/${el.innerHTML}.jpg`);
+      cap.style.opacity = 1;
+    }, 500);
+  } else {
+    clearTimeout(imgto);
+    imgto = setTimeout(() => {
+      cap.setAttribute("src", `assets/events/event-1.jpg`);
+      cap.style.opacity = 1;
+    }, 500);
+  }
+});
+let prevpos = 0;
+document.addEventListener("mousemove", e => {
+  const pos = ul.getBoundingClientRect().y;
+  let direction;
+  if (prevpos > pos) direction = "up";
+  else direction = "down";
+  prevpos = pos;
+  ul.style.transform =
+    "translateY(" + -(e.clientY - screen.height / 2 + 100) + "px)";
+
+  ar_li.forEach(l => {
+    if (direction === "up") l.style.transform = "rotate3d(1, 1, 1, -3deg)";
+    if (direction === "down") l.style.transform = "rotate3d(1,1,1,3deg)";
   });
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    ar_li.forEach(l => {
+      l.style.transform = "scaleY(1) rotate3d(0, 0, 0, 0)";
+    });
+  }, 80);
+});
+
+ul.addEventListener("click", e => {
+  if (e.target.tagName === "LI") {
+    targetClicked = e.target;
+    modal.open();
+  }
+});
